9582065898b535cb344dd94c927e0cb0783496d8
Abhijit4001 <abhijitdarpan2004@gmail.com>
2026-05-12 21:36:13 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,13 +4,14 @@ import viteReact from '@vitejs/plugin-react'
 import viteTsConfigPaths from 'vite-tsconfig-paths'
 import tailwindcss from '@tailwindcss/vite'
 import contentCollections from '@content-collections/vite'
+import contentCollections from '@content-collections/vite'
 
 export default defineConfig({
   plugins: [
     contentCollections(),
-    viteTsConfigPaths({ projects: ['./tsconfig.json'] }),
+     viteTsConfigPaths({ projects: ['./tsconfig.json'] }),
     tailwindcss(),
-    tanstackStart(),
+     tanstackStart(),
     viteReact(),
   ],
 })
